5a6064f7fbcd89fdf61dc265caa196071a97d0d9
ign-1997 <gazelev6@yandex.ru>
2020-02-28 12:27:27 +0300

    Новый сплеш и иконка движка
    Просто поменял эти файлы.

--- engine.vc2008/Layers/xrRender/FTreeVisual.cpp
+++ engine.vc2008/Layers/xrRender/FTreeVisual.cpp
@@ -103,7 +103,9 @@ struct	FTreeVisual_setup
 
 		// Calc wind-vector3, scale
 		float	tm_rot			= PI_MUL_2*Device.fTimeGlobal/ps_r__Tree_w_rot;
+
 		wind.set				(_sin(tm_rot),0,_cos(tm_rot),0);	wind.normalize	();	wind.mul(ps_r__Tree_w_amp);	// dir1*amplitude
+
 		scale					= 1.f/float(FTreeVisual_quant);
 
 		// setup constants
