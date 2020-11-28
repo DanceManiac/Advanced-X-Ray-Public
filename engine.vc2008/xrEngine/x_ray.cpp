e0e767cf5a87c6af0a393f48408f8c02283b3a9b
ign-1997 <gazelev6@yandex.ru>
2020-11-28 10:16:04 +0300

    Убрана привязка процесса игры к первому ядру

--- engine.vc2008/xrEngine/x_ray.cpp
+++ engine.vc2008/xrEngine/x_ray.cpp
@@ -758,8 +758,6 @@ int APIENTRY WinMain_impl(HINSTANCE hInstance,
 	g_dedicated_server			= true;
 #endif // DEDICATED_SERVER
 
-	SetThreadAffinityMask		(GetCurrentThread(),1);
-
 	// Title window
 	logoWindow					= CreateDialog(GetModuleHandle(NULL),	MAKEINTRESOURCE(IDD_STARTUP), 0, logDlgProc );
 	
