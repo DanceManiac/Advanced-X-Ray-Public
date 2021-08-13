c875315d6cd496b43de29ecacdb147f7899ab547
ign-1997 <gazelev6@yandex.ru>
2021-08-13 21:00:57 +0300

    Мобы не должны видеть дальше дистанции видимости
    Нашёл в OGSR Engine. Автор: dsh2dsh.

--- engine.vc2008/xrGameCS/CustomMonster.cpp
+++ engine.vc2008/xrGameCS/CustomMonster.cpp
@@ -588,9 +588,9 @@ void CCustomMonster::update_range_fov	(float &new_range, float &new_fov, float s
 {
 	const float	standard_far_plane			= eye_range;
 
-	float	current_fog_density				= GamePersistent().Environment().CurrentEnv->fog_density	;	
+	float	current_fog_density				= GamePersistent().Environment().CurrentEnv->fog_density;	
 	// 0=no_fog, 1=full_fog, >1 = super-fog
-	float	current_far_plane				= GamePersistent().Environment().CurrentEnv->far_plane	;	
+	float current_far_plane					= GamePersistent().Environment().CurrentEnv->fog_far;
 	// 300=standart, 50=super-fog
 
 	new_fov									= start_fov;
@@ -611,6 +611,7 @@ void CCustomMonster::update_range_fov	(float &new_range, float &new_fov, float s
 			)
 		)
 	;
+	clamp(new_range, 0.f, current_far_plane);
 }
 
 void CCustomMonster::eye_pp_s1			()
