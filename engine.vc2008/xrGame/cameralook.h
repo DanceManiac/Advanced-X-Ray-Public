b755f6a021c50508ce54fc0e5f1cdba72b195833
ign-1997 <gazelev6@yandex.ru>
2021-08-15 21:31:24 +0300

    Удалено автоприцеливание в режиме от третьего лица
    Спасибо ggGhosTt за предоставленную правку.

--- engine.vc2008/xrGame/cameralook.h
+++ engine.vc2008/xrGame/cameralook.h
@@ -31,9 +31,6 @@ public:
 	static Fvector	m_cam_offset;
 protected:
 	CObject*		m_locked_enemy;
-	Fvector2		m_autoaim_inertion_yaw;
-	Fvector2		m_autoaim_inertion_pitch;
-	void			UpdateAutoAim	();
 public:
 					CCameraLook2	( CObject* p, u32 flags=0):CCameraLook(p, flags){m_locked_enemy=NULL;};
 	virtual			~CCameraLook2	(){}
