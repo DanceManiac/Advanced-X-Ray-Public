20d4e3b2dc8b237f00ad6040e019d18e9956b054
ign-1997 <gazelev6@yandex.ru>
2022-06-07 19:15:09 +0300

    Убран хит по дверям автомобилей

--- SourcesAXR/xrGame/Car.cpp
+++ SourcesAXR/xrGame/Car.cpp
@@ -557,7 +557,7 @@ void	CCar::Hit							(SHit* pHDS)
 	//	CExplosive::SetInitiator(HDS.who->ID());
 	//}
 	WheelHit(HDS.damage(),HDS.bone(),HDS.hit_type);
-	DoorHit(HDS.damage(),HDS.bone(),HDS.hit_type);
+	//DoorHit(HDS.damage(),HDS.bone(),HDS.hit_type);
 	float hitScale=1.f,woundScale=1.f;
 	if(HDS.hit_type!=ALife::eHitTypeStrike) CDamageManager::HitScale(HDS.bone(), hitScale, woundScale);
 	HDS.power *= GetHitImmunity(HDS.hit_type)*hitScale;
