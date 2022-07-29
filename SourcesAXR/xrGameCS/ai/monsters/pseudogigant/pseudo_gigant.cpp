177bd64e202168b3857346e37655b8b35bd28295
Dance Maniac <gazelev6@yandex.ru>
2022-07-29 18:32:16 +0300

    Мутанты не трогают квестовые предметы
    Взял с BttR.

--- SourcesAXR/xrGameCS/ai/monsters/pseudogigant/pseudo_gigant.cpp
+++ SourcesAXR/xrGameCS/ai/monsters/pseudogigant/pseudo_gigant.cpp
@@ -238,9 +238,13 @@ void CPseudoGigant::on_threaten_execute()
 	// נאחבנמסטעü מבתוךעû
 	m_nearest.clear_not_free		();
 	Level().ObjectSpace.GetNearest	(m_nearest,Position(), 15.f, NULL); 
-	for (u32 i=0;i<m_nearest.size();i++) {
+	for (u32 i=0;i<m_nearest.size();i++) 
+	{
 		CPhysicsShellHolder  *obj = smart_cast<CPhysicsShellHolder *>(m_nearest[i]);
-		if (!obj || !obj->m_pPhysicsShell) continue;
+		if (!obj || !obj->m_pPhysicsShell || 
+			(obj->spawn_ini() && obj->spawn_ini()->section_exist("ph_heavy")) ||
+			(pSettings->line_exist(obj->cNameSect().c_str(), "ph_heavy") && pSettings->r_bool(obj->cNameSect().c_str(), "ph_heavy")) ||
+			(pSettings->line_exist(obj->cNameSect().c_str(), "quest_item") && pSettings->r_bool(obj->cNameSect().c_str(), "quest_item"))) continue;
 
 		Fvector dir;
 		Fvector pos;
