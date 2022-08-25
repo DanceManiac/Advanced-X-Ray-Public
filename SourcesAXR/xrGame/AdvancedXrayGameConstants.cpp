9f906faa2cfda661cfc078f5767c922e284cd43a
Hrusteckiy <kostia4554t@yandex.ru>
2022-08-25 23:55:48 +0300

    wut

--- SourcesAXR/xrGame/AdvancedXrayGameConstants.cpp
+++ SourcesAXR/xrGame/AdvancedXrayGameConstants.cpp
@@ -32,7 +32,7 @@ int		m_iArtefactsCount = 5;
 float	m_fDistantSndDistance = 150.f;
 float	m_fDistantSndDistanceFar = 250.f;
 Fvector4 m_FV4RedColor = Fvector4().set(255, 0, 0, 255);
-Fvector4 m_FV4GreenColor = Fvector4().set(0, 255, 255);
+Fvector4 m_FV4GreenColor = Fvector4().set(0, 255, 255, 255);
 
 namespace GameConstants
 {
@@ -67,8 +67,8 @@ namespace GameConstants
 		m_fDistantSndDistance = READ_IF_EXISTS(pAdvancedSettings, r_float, "gameplay", "distant_snd_distance", 150.f);
 		m_fDistantSndDistanceFar = READ_IF_EXISTS(pAdvancedSettings, r_float, "gameplay", "distant_snd_distance_far", 250.f);
 		m_bColorizeValues = READ_IF_EXISTS(pAdvancedSettings, r_bool, "gameplay", "colorize_values", false);
-		m_FV4RedColor = READ_IF_EXISTS(pAdvancedSettings, r_fvector4, "gameplay", "colorize_values_red", Fvector4().set(255, 0, 0,255));
-		m_FV4GreenColor = READ_IF_EXISTS(pAdvancedSettings, r_fvector4, "gameplay", "colorize_values_green", Fvector4().set(0, 255, 255));
+		m_FV4RedColor = READ_IF_EXISTS(pAdvancedSettings, r_fvector4, "gameplay", "colorize_values_red", Fvector4().set(255, 0, 0, 255));
+		m_FV4GreenColor = READ_IF_EXISTS(pAdvancedSettings, r_fvector4, "gameplay", "colorize_values_green", Fvector4().set(0, 255, 255, 255));
 
 		Msg("# Advanced X-Ray GameConstants are loaded");
 	}
