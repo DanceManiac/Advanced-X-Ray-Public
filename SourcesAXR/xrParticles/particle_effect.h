3d2532dca15d8180c37863f95a7e2e8c541fe07c
Dance Maniac <gazelev6@yandex.ru>
2022-11-04 17:22:41 +0300

    Фиксы партиклов с OpenXRay
    1.
    https://github.com/OpenXRay/xray-16/commit/13d2e015e0ba15d1bebcb8f12cb632846c9a4103
    
    2.
    https://github.com/OpenXRay/xray-16/commit/20032e91a537603e6bafb4a2bf7145d556ce77e7
    
    3.
    https://github.com/OpenXRay/xray-16/commit/252a73584fac154aaa70fba2ccec3aa4ca3be9d8

--- SourcesAXR/xrParticles/particle_effect.h
+++ SourcesAXR/xrParticles/particle_effect.h
@@ -2,7 +2,8 @@
 #ifndef particle_effectH
 #define particle_effectH
 
-namespace PAPI{
+namespace PAPI
+{
 	// A effect of particles - Info and an array of Particles
 	struct ParticleEffect
 	{
@@ -10,7 +11,6 @@ namespace PAPI{
 		u32			max_particles;			// Max particles allowed in effect.
 		u32			particles_allocated;	// Actual allocated size.
 		Particle*	particles;				// Actually, num_particles in size
-		void*		real_ptr;				// Base, possible not aligned pointer
         OnBirthParticleCB 	b_cb;
         OnDeadParticleCB	d_cb;
         void*				owner;
@@ -27,14 +27,12 @@ namespace PAPI{
 			max_particles			= mp;
 			particles_allocated		= max_particles;
 
-			real_ptr				= xr_malloc( sizeof( Particle ) * ( max_particles + 1 ) );
-			particles				= (Particle*) ( (DWORD) real_ptr + ( 64 - ( (DWORD) real_ptr & 63 ) ) );
+			particles = xr_alloc<Particle>(max_particles);
 			//Msg( "Allocated %u bytes (%u particles) with base address 0x%p" , max_particles * sizeof( Particle ) , max_particles , particles );
 		}
-					~ParticleEffect	()
-		{
-			xr_free					(real_ptr);
-		}
+
+		~ParticleEffect() { xr_free(particles); }
+
 		IC int		Resize			(u32 max_count)
 		{
 			// Reducing max.
@@ -50,22 +48,20 @@ namespace PAPI{
 			}
 
 			// Allocate particles.
-			void* new_real_ptr = xr_malloc( sizeof( Particle ) * ( max_count + 1 ) );
-
-			if( new_real_ptr == NULL ){
+			Particle* new_particles = xr_alloc<Particle>(max_count);
+			if (!new_particles)
+			{
 				// ERROR - Not enough memory. Just give all we've got.
 				max_particles		= particles_allocated;
 				return max_particles;
 			}
 
-			Particle* new_particles	= (Particle*) ( (DWORD) new_real_ptr + ( 64 - ( (DWORD) new_real_ptr & 63 ) ) );
-			//Msg( "Re-allocated %u bytes (%u particles) with base address 0x%p" , max_count * sizeof( Particle ) , max_count , new_particles );
+			//Msg("Re-allocated %u bytes (%u particles) with base address 0x%p", max_count * sizeof(Particle), max_count, new_particles);
 
 			CopyMemory			(new_particles, particles, p_count * sizeof(Particle));
-			xr_free					(real_ptr);
+			xr_free(particles);
 
 			particles				= new_particles;
-			real_ptr				= new_real_ptr;
 
 			max_particles			= max_count;
 			particles_allocated		= max_count;
