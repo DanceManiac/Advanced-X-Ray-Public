89d0d171c4eef12e6132e469f9e214f1f84fa5c9
Dance Maniac <gazelev6@yandex.ru>
2022-11-23 21:05:58 +0300

    Доработка Position Informer
    Thx @ggGhosTt

--- SourcesAXR/xrGameCS/embedded_editor/embedded_editor_pos_informer.cpp
+++ SourcesAXR/xrGameCS/embedded_editor/embedded_editor_pos_informer.cpp
@@ -5,36 +5,26 @@
 #include "embedded_editor_pos_informer.h"
 #include "embedded_editor_helper.h"
 #include <imgui.h>
+#include <fstream>
 
 string256 section_name = "section";
 
 void SavePosition(string256 sect)
 {
-    CInifile f("position_info", false, true, true);
-    string_path section{};
-    if (f.section_exist(sect))
-    {
-        int sect_id = 2;
-        strconcat(sizeof(section), section, sect, "_", std::to_string(sect_id).c_str());
-        while (f.section_exist(section))
-        {
-            delete_data(section);
-            sect_id++;
-            strconcat(sizeof(section), section, sect, "_", std::to_string(sect_id).c_str());
-        }
-    }
-    else
-        strconcat(sizeof(section), section, sect, "");
+	std::string userDir = FS.get_path("$app_data_root$")->m_Path, fileToWrite = "//position_info.txt";
+	std::string path = userDir + fileToWrite;
+	std::fstream informer;
 
-    f.w_fvector3(section, "position", Actor()->Position());
-    f.w_fvector3(section, "direction", Actor()->Direction());
-    f.w_u32(section, "game_vertex_id", Actor()->ai_location().game_vertex_id());
-    f.w_u32(section, "level_vertex_id", Actor()->ai_location().level_vertex_id());
+	informer.open(path, std::fstream::app);
 
-    string_path fileName;
-    FS.update_path(fileName, "$app_data_root$", "position_info");
-    strconcat(sizeof(fileName), fileName, fileName, ".txt");
-    f.save_as(fileName);
+	if (informer.is_open())
+	{
+		informer << "[" << sect << "]" << "\n";
+		informer << "position = " << Actor()->Position().x << ", " << Actor()->Position().y << ", " << Actor()->Position().z << "\n";
+		informer << "direction = " << Actor()->Direction().x << ", " << Actor()->Direction().y << ", " << Actor()->Direction().z << "\n";
+		informer << "game_vertex_id = " << Actor()->ai_location().game_vertex_id() << "\n";
+		informer << "level_vertex_id = " << Actor()->ai_location().level_vertex_id() << "\n\n";
+	}
 }
 
 void ShowPositionInformer(bool& show)
@@ -50,12 +40,12 @@ void ShowPositionInformer(bool& show)
 	int actor_level_vertex = Actor()->ai_location().level_vertex_id();
 
 	ImGui::InputText("section name", (char*)&section_name, 256);
-	ImGui::InputFloat3("actor_position", (float*)&actor_position);
-	ImGui::InputFloat3("actor_direction", (float*)&actor_direction);
-	ImGui::InputInt("actor_game_vertex_id", (int*)&actor_game_vertex);
-	ImGui::InputInt("actor_level_vertex_id", (int*)&actor_level_vertex);
+	ImGui::InputFloat3("position", (float*)&actor_position);
+	ImGui::InputFloat3("direction", (float*)&actor_direction);
+	ImGui::InputInt("game_vertex_id", (int*)&actor_game_vertex);
+	ImGui::InputInt("level_vertex_id", (int*)&actor_level_vertex);
 
-	if (ImGui::Button("Save")) 
+	if (ImGui::Button("Save"))
 	{
 		SavePosition(section_name);
 	}
