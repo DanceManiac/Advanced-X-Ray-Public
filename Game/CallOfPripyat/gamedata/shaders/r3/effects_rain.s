2a60ef0f7758554cdba98be44967de48020fb273
Dance Maniac <gazelev6@yandex.ru>
2022-11-26 17:53:17 +0300

    [DX10+] Фикс чёрного дождя

--- Game/CallOfPripyat/gamedata/shaders/r3/effects_rain.s
+++ Game/CallOfPripyat/gamedata/shaders/r3/effects_rain.s
@@ -7,9 +7,9 @@ function normal(shader, t_base, t_second, t_detail)
 	
 	shader : dx10texture("s_base", t_base)
 	shader : dx10texture("s_bump", t_base.."_bump")	
-	shader : dx10texture("s_image", "$user$genericpp")
-	shader : dx10texture("env_s0", "$user$env_s0")
-	shader : dx10texture("env_s1", "$user$env_s1")
+	shader : dx10texture("s_image", "$user$generic_temp")
+	shader : dx10texture("sky_s0", "$user$sky0")
+	shader : dx10texture("sky_s1", "$user$sky1")
 	  
 	shader : dx10sampler("smp_base")
 	shader : dx10sampler("smp_rtlinear")
