9323880f1299b6b8fc963c9fac5a136e55f67811
Dance Maniac <gazelev6@yandex.ru>
2023-01-04 23:54:14 +0300

    Фикс невнимательности

--- SourcesAXR/xrGameCS/player_hud.h
+++ SourcesAXR/xrGameCS/player_hud.h
@@ -259,7 +259,6 @@ struct attachable_hud_item
 //props
 	u32								m_upd_firedeps_frame;
 	void		tune				(Ivector values);
-	void		SaveCfg				(const int idx) const;
 	u32			anim_play			(const shared_str& anim_name, BOOL bMixIn, const CMotionDef*& md, u8& rnd);
 
 };
@@ -298,6 +297,7 @@ public:
 
 	void			calc_transform		(u16 attach_slot_idx, const Fmatrix& offset, Fmatrix& result);
 	void			tune				(Ivector values);
+	void			SaveCfg				(const int idx) const;
 	u32				motion_length		(const MotionID& M, const CMotionDef*& md, float speed);
 	u32				motion_length		(const shared_str& anim_name, const shared_str& hud_name, const CMotionDef*& md);
 	u32				motion_length_script(LPCSTR section, LPCSTR anm_name, float speed);
