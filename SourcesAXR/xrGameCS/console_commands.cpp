53a893a4814ef18778cb28107bff0f95d785f61c
Dance Maniac <gazelev6@yandex.ru>
2023-03-16 22:18:24 +0300

    Фикс команды ui_textures_for_prefetching

--- SourcesAXR/xrGameCS/console_commands.cpp
+++ SourcesAXR/xrGameCS/console_commands.cpp
@@ -413,6 +413,15 @@ public:
 	}
 };
 
+struct DumpTxrsForPrefetching : public IConsole_Command {
+	DumpTxrsForPrefetching(LPCSTR N) : IConsole_Command(N) { bEmptyArgsHandled = true; };
+
+	virtual void Execute(LPCSTR args)
+	{
+		MainMenu()->ReportTxrsForPrefetching();
+	}
+};
+
 class CCC_GameDifficulty : public CCC_Token {
 public:
 	CCC_GameDifficulty(LPCSTR N) : CCC_Token(N,(u32*)&g_SingleGameDifficulty,difficulty_type_token)  {};
@@ -1103,15 +1112,6 @@ public:
 	};
 };
 #endif // #if defined(USE_DEBUGGER) && defined(USE_LUA_STUDIO)
-
-struct DumpTxrsForPrefetching : public IConsole_Command {
-	DumpTxrsForPrefetching(LPCSTR N) : IConsole_Command(N) { bEmptyArgsHandled = true; };
-
-	virtual void Execute(LPCSTR args)
-	{
-		MainMenu()->ReportTxrsForPrefetching();
-	}
-};
 
 class CCC_DumpInfos : public IConsole_Command {
 public:
@@ -2101,9 +2101,7 @@ CMD4(CCC_Integer,			"hit_anims_tune",						&tune_hit_anims,		0, 1);
 		CMD1(CCC_Disinfo,		"d_info");
 		CMD1(CCC_GiveTask,		"g_task");
 		CMD1(CCC_GiveMoney,		"g_money");
-#ifdef DEBUG
 		CMD1(DumpTxrsForPrefetching, "ui_textures_for_prefetching");//Prints the list of UI textures, which caused stutterings during game
-#endif
 		CMD3(CCC_Mask,			"g_god",			&psActorFlags,	AF_GODMODE);
 		CMD3(CCC_Mask,			"g_unlimitedammo",	&psActorFlags,	AF_UNLIMITEDAMMO);
 		CMD4(CCC_Integer,		"hud_adjust_mode",	&hud_adj_mode,	0, 5);
