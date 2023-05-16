4561f261eab1c12bc600f36c7b47a53efc659261
Hrusteckiy <kostia4554t@yandex.ru>
2023-05-16 03:16:24 +0300

    ЧН: фикс перепутанного порядка стат костюмов.
    В енуме типов урона `strike` идёт после `burn`, а `shock` - по итогу на втором месте. Из-за этого в статах костюмов мы видим название и иконку для одних стат, а отображались фактически другие.

--- SourcesAXR/xrGameCS/ui/UIOutfitInfo.cpp
+++ SourcesAXR/xrGameCS/ui/UIOutfitInfo.cpp
@@ -15,8 +15,8 @@
 LPCSTR immunity_names[]=
 {
 	"burn_immunity",
-	"strike_immunity",
 	"shock_immunity",
+	"strike_immunity",
 	"wound_immunity",		
 	"radiation_immunity",
 	"telepatic_immunity",
@@ -28,8 +28,8 @@ LPCSTR immunity_names[]=
 LPCSTR immunity_st_names[]=
 {
 	"ui_inv_outfit_burn_protection",
-	"ui_inv_outfit_strike_protection",
 	"ui_inv_outfit_shock_protection",
+	"ui_inv_outfit_strike_protection",
 	"ui_inv_outfit_wound_protection",
 	"ui_inv_outfit_radiation_protection",
 	"ui_inv_outfit_telepatic_protection",
