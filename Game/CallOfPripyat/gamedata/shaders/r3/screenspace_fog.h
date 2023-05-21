78472281f0d482ea53fd9772d0fbabbd36faf844
Dance Maniac <gazelev6@yandex.ru>
2023-05-21 10:31:22 +0300

    [DX11]: SSS Update 15.4 - Fog Update

--- Game/CallOfPripyat/gamedata/shaders/r3/screenspace_fog.h
+++ Game/CallOfPripyat/gamedata/shaders/r3/screenspace_fog.h
@@ -1,7 +1,7 @@
 /**
- * @ Version: SCREEN SPACE SHADERS - UPDATE 13.2
+ * @ Version: SCREEN SPACE SHADERS - UPDATE 14.4
  * @ Description: 2 Layers fog ( Distance + Height )
- * @ Modified time: 2022-12-28 02:09
+ * @ Modified time: 2023-01-30 09:14
  * @ Author: https://www.moddb.com/members/ascii1457
  * @ Mod: https://www.moddb.com/mods/stalker-anomaly/addons/screen-space-shaders
  */
@@ -28,9 +28,9 @@ float SSFX_HEIGHT_FOG(float3 P, float World_Py, inout float3 color)
 	float fogheight = smoothstep(G_FOG_HEIGHT, -G_FOG_HEIGHT, World_Py) * G_FOG_HEIGHT_INTENSITY;
 #endif
 	// Add the height fog to the distance fog
-	float fogresult = saturate(fog + fogheight * fog);
+	float fogresult = saturate(fog + fogheight * fog * G_FOG_HEIGHT_INTENSITY);
 
-	// Blend factor to mix sun color, fog color and adjust intensity.
+	// Blend factor to mix sun color and fog color. Adjust intensity to.
 	float FogBlend = fogheight * G_FOG_SUNCOLOR_INTENSITY;
 
 	// Final fog color
@@ -47,14 +47,13 @@ float SSFX_FOGGING(float Fog, float World_Py)
 {
 	// Height fog
 #ifdef G_USE_PARAMS_FROM_WEATHER
-	float fog_height = smoothstep(lowland_fog_params.x, -lowland_fog_params.x, World_Py);
+	float fog_height = smoothstep(lowland_fog_params.x, -lowland_fog_params.x, World_Py) * G_FOG_HEIGHT_INTENSITY;
 #else
-	float fog_height = smoothstep(G_FOG_HEIGHT, -G_FOG_HEIGHT, World_Py);
+	float fog_height = smoothstep(G_FOG_HEIGHT, -G_FOG_HEIGHT, World_Py) * G_FOG_HEIGHT_INTENSITY;
 #endif
 
 	// Add height fog to default distance fog.
-	float fog_extra = saturate(Fog + fog_height * Fog);
-	
-	// Return distance fog & height fog.
+	float fog_extra = saturate(Fog + fog_height * (Fog * G_FOG_HEIGHT_DENSITY));
+
 	return 1.0f - fog_extra;
 }
