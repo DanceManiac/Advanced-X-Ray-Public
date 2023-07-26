93b6a56cb77badf4cea8bd9e5de56e7dac49811b
Dance Maniac <50047331+DanceManiac@users.noreply.github.com>
2023-07-26 17:23:15 +0300

    [DX11]: Возвращён фикс Motion Blur

--- Game/ClearSky/gamedata/shaders/r3/mblur.h
+++ Game/ClearSky/gamedata/shaders/r3/mblur.h
@@ -1,48 +1,49 @@
 #ifndef	MBLUR_H
 #define MBLUR_H
-
-#ifndef 	USE_MBLUR
-float3 	mblur	(float2 UV, float3 pos, float3 c_original)	{ return c_original; }
-#else
+#define MBLUR_WPN
 #include "common.h"
 
-float3 	mblur	(float2 UV, float3 pos, float3 c_original)	{ return c_original; }
-//	TODO: DX10: Reimplement
-//	Currently disabled.
-/*
+#ifndef USE_MBLUR
+float3 mblur(float2 UV, float3 pos, float3 c_original)
+{
+	return c_original;
+}
+#else
+
 uniform float4x4	m_current;
 uniform float4x4	m_previous;
-uniform float2 	m_blur;		// scale_x / 12, scale_y / 12
+uniform float2		m_blur;		// x=scale_x/12, y=scale_y/12
 
-#define MBLUR_SAMPLES 	float(12)
-#define MBLUR_CLAMP	float(0.001)
+#define MBLUR_SAMPLES	float(12.0f)
+#define MBLUR_CLAMP		float(0.001f)
 
-float3 	mblur	(float2 UV, float3 pos, float3 c_original)	
+float3 mblur(float2 UV, float3 pos, float3 c_original)
 {
-	float4 	pos4		= float4	(pos,1.h);
+	// The sky depth value is 0 since we're rendering it without z-test
+	if (pos.z < 0.00001f)
+		pos.z = SKY_DEPTH;
 
-	float4 	p_current	= mul	(m_current,	pos4);
-	float4 	p_previous 	= mul	(m_previous,	pos4);
-	float2 	p_velocity 	= m_blur * ( (p_current.xy/p_current.w)-(p_previous.xy/p_previous.w) );
-		p_velocity	= clamp	(p_velocity,-MBLUR_CLAMP,+MBLUR_CLAMP);
+	float4	pos4		= float4(pos, 1.0f);
+
+	float4	p_current	= mul(m_current, pos4);
+	float4	p_previous 	= mul(m_previous, pos4);
+	float2	p_velocity 	= m_blur * ((p_current.xy/p_current.w) - (p_previous.xy/p_previous.w));
+			p_velocity	= clamp(p_velocity, -MBLUR_CLAMP, +MBLUR_CLAMP);
+			
+	//Small hud attenuation
+	#ifdef MBLUR_WPN
+    if (pos.z < 2.0) // disable for hud
+        p_velocity = 0.0;
+	#endif
 
 	// For each sample, sum up each sample's color in "Blurred" and then divide
 	// to average the color after all the samples are added.
-	float3 	blurred 	= 	c_original	;
-        	blurred 	+= 	tex2D(s_image, p_velocity * 1.h  + UV).rgb;
-		blurred		+= 	tex2D(s_image, p_velocity * 2.h  + UV).rgb;
-		blurred		+= 	tex2D(s_image, p_velocity * 3.h  + UV).rgb;
-		blurred		+= 	tex2D(s_image, p_velocity * 4.h  + UV).rgb;
-        	blurred 	+= 	tex2D(s_image, p_velocity * 5.h  + UV).rgb;
-		blurred		+= 	tex2D(s_image, p_velocity * 6.h  + UV).rgb;
-		blurred		+= 	tex2D(s_image, p_velocity * 7.h  + UV).rgb;
-		blurred		+= 	tex2D(s_image, p_velocity * 8.h  + UV).rgb;
-        	blurred 	+= 	tex2D(s_image, p_velocity * 9.h  + UV).rgb;
-		blurred		+= 	tex2D(s_image, p_velocity * 10.h + UV).rgb;
-		blurred		+= 	tex2D(s_image, p_velocity * 11.h + UV).rgb;
-	return 	blurred/MBLUR_SAMPLES;
-}
-*/
-#endif
+	float3	blurred 	= c_original;
+	
+	for (int i = 1; i < MBLUR_SAMPLES; ++i)
+		blurred += s_image.Sample(smp_rtlinear, p_velocity * i + UV).xyz;
 
+	return blurred/MBLUR_SAMPLES;
+}
 #endif
+#endif // MBLUR_H
