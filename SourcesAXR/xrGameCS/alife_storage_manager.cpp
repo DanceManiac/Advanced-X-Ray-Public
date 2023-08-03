2631ca82f780bd78475518fc72531f3e9468b7ba
Hrusteckiy <kostia4554t@yandex.ru>
2023-08-03 22:47:39 +0300

    Фикс пропажи кнопки "последнее сохранение".
    Начиная в ЧН, наши любимые гении воткнули на деструкторе алайфа превращение значения последнего сохранения в нуль. Вследствии этого, при корректном выходе из игры(не вылет и не убийство процесса), из `user.ltx` пропадала строка `load_last_save *savename*`, сама же кнопка в меню напрямую зависит от строки в данном файле.

--- SourcesAXR/xrGameCS/alife_storage_manager.cpp
+++ SourcesAXR/xrGameCS/alife_storage_manager.cpp
@@ -31,7 +31,6 @@ extern string_path g_last_saved_game;
 
 CALifeStorageManager::~CALifeStorageManager	()
 {
-	*g_last_saved_game			= 0;
 }
 
 void CALifeStorageManager::save	(LPCSTR save_name, bool update_name)
