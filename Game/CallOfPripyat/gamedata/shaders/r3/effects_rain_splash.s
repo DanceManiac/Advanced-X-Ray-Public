834e6e21e09d179c33c00de3a3802f7b52e70679
Dance Maniac <50047331+DanceManiac@users.noreply.github.com>
2023-08-09 15:23:28 +0300

    [DX11]: Screen Space Shaders - Update 17 (Textures, Shaders)
    Автор адаптации: @yohjimane

--- Game/CallOfPripyat/gamedata/shaders/r3/effects_rain_splash.s
+++ Game/CallOfPripyat/gamedata/shaders/r3/effects_rain_splash.s
@@ -4,16 +4,15 @@
 -- @ Mod: https://www.moddb.com/mods/stalker-anomaly/addons/screen-space-shaders
 
 function normal(shader, t_base, t_second, t_detail)
-	shader	: begin	("effects_rain","effects_rain")
+	shader	: begin	("effects_rain","effects_rain_splash")
 			: zb	(true,false)
 			: blend	(true,blend.srcalpha,blend.invsrcalpha)
 			: aref 		(true,0)
+			 
+	shader : dx10texture("s_base", "shaders\\rain_splash")
+	shader : dx10texture("s_rimage", "$user$generic_temp")
 
-	shader : dx10texture("s_base", "shaders\\rain_drop")
-	shader : dx10texture("s_position", "$user$position")
-	shader : dx10texture("s_accumulator", "$user$accum")
-	shader : dx10texture("s_blur_4", "$user$blur_4")
 
-	shader 	: dx10sampler	("smp_rtlinear")
+	shader 	: dx10sampler	("smp_base")
 	shader 	: dx10sampler	("smp_nofilter")
 end
--- Game/ClearSky/gamedata/shaders/r3/effects_rain.s
+++ Game/ClearSky/gamedata/shaders/r3/effects_rain.s
@@ -1,16 +1,19 @@
-function normal		(shader, t_base, t_second, t_detail)
-	shader : begin("forward_rain","forward_rain")
-	: zb(true,false)
-	: blend(true,blend.srcalpha,blend.invsrcalpha)
-	: aref(true,0)
-	: sorting(2, true)	
-	
-	shader : dx10texture("s_base", t_base)
-	shader : dx10texture("s_bump", t_base.."_bump")	
-	shader : dx10texture("s_image", "$user$generic_temp")
-	shader : dx10texture("sky_s0", "$user$sky0")
-	shader : dx10texture("sky_s1", "$user$sky1")
-	  
-	shader : dx10sampler("smp_base")
-	shader : dx10sampler("smp_rtlinear")
+-- @ Version: SCREEN SPACE SHADERS - UPDATE 17
+-- @ Description: Rain - Shader Config
+-- @ Author: https://www.moddb.com/members/ascii1457
+-- @ Mod: https://www.moddb.com/mods/stalker-anomaly/addons/screen-space-shaders
+
+function normal(shader, t_base, t_second, t_detail)
+	shader	: begin	("effects_rain","effects_rain")
+			: zb	(true,false)
+			: blend	(true,blend.srcalpha,blend.invsrcalpha)
+			: aref 		(true,0)
+
+	shader : dx10texture("s_base", "shaders\\rain_drop")
+	shader : dx10texture("s_position", "$user$position")
+	shader : dx10texture("s_accumulator", "$user$accum")
+	shader : dx10texture("s_blur_4", "$user$blur_4")
+
+	shader 	: dx10sampler	("smp_rtlinear")
+	shader 	: dx10sampler	("smp_nofilter")
 end
