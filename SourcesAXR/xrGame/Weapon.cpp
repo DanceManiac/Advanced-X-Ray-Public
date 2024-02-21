b911f1803c1de52a54de9ebb58c9dc225637979f
Dance Maniac <gazelev6@yandex.ru>
2024-02-21 12:24:12 +0300

    Фикс оффсетов, если прицел не установлен
    Их не надо в таком случае каждый раз обновлять.

--- SourcesAXR/xrGame/Weapon.cpp
+++ SourcesAXR/xrGame/Weapon.cpp
@@ -3836,7 +3836,9 @@ void CWeapon::UpdateAimOffsets()
 	shared_str cur_scope_sect = (m_sScopeAttachSection.size() ? m_sScopeAttachSection : (m_eScopeStatus == ALife::eAddonAttachable) ? m_scopes[m_cur_scope].c_str() : "scope");
 	psHUD_FOV_def = last_hud_fov;
 
-	if (!IsScopeAttached() || (!IsZoomed() && !IsRotatingFromZoom()) || !cur_scope_sect.size())
+	static bool bNeedRestoreOffsets = false;
+
+	if (bNeedRestoreOffsets && (!IsScopeAttached() || (!IsZoomed() && !IsRotatingFromZoom()) || !cur_scope_sect.size()))
 	{
 		attachable_hud_item* hi = HudItemData();
 
@@ -3862,6 +3864,8 @@ void CWeapon::UpdateAimOffsets()
 		hi->m_measures.m_hands_offset[0][2] = pSettings->r_fvector3(hud_sect, val_name);
 		strconcat(sizeof(val_name), val_name, "gl_hud_offset_rot", _prefix);
 		hi->m_measures.m_hands_offset[1][2] = pSettings->r_fvector3(hud_sect, val_name);
+
+		bNeedRestoreOffsets = false;
 
 		return;
 	}
@@ -3901,5 +3905,7 @@ void CWeapon::UpdateAimOffsets()
 		hi->m_measures.m_hands_offset[0][2] = pSettings->r_fvector3(cur_scope_sect, val_name);
 		strconcat(sizeof(val_name), val_name, "gl_hud_offset_rot", _prefix);
 		hi->m_measures.m_hands_offset[1][2] = pSettings->r_fvector3(cur_scope_sect, val_name);
+
+		bNeedRestoreOffsets = true;
 	}
 }
