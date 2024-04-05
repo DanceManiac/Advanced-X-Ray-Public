a9ac6b8c83353c0082fb3278d5a64460957f6918
Dance Maniac <gazelev6@yandex.ru>
2024-04-06 01:49:19 +0300

    Cкриптовое хранилище из xp-dev
    Использование хранилища:
    
    local xvars = get_stored_vars()
    xvars.test = 123
    xvars.test2 = {}
    xvars.test2.test1 = 456
    
    Все просто, использовать, как стандартную таблицу. Есть пара
    особенностей. Хранить лучше только простые типы: строки, числа,
    таблицы и т.п, в том числе вложенные. Объекты, вектора, время и т.п.,
    то, что является userdata, лучше не использовать.
    
    Вторая особенность - списки. Для прямого доступа к элементам списка
    ничего делать не нужно, но если захочется с этим списком работать, как
    с настоящим списком, т.к. итерировать, к примеру, или подсунуть его в
    unpack(), то его нужно превратить в lua таблицу. Например
    
    xvars.test = { 1, 2, 3 }
    local t = xvars.test()
    for i, v in ipairs( t ) do
    end
    
    Операция выполнения, т.е. (), возвращает lua table, с которой уже
    можно делать, что угодно. По умолчанию, преобразование не рекурсивное,
    т.е. не затронет вложенные таблицы. Что бы сделать его рекурсивным,
    нужно сделать вот так
    
    local t = xvars.test( true )
    
    т.е. передать параметр.
    
    Взял тут:
    
    https://github.com/OGSR/OGSR-Engine/commit/06564e0acf4460d8c8ebe0dd918343215598d627

--- SourcesAXR/build_config_defines.h
+++ SourcesAXR/build_config_defines.h
@@ -1,9 +1,12 @@
 ﻿#pragma once
 
 //Renderer
-	#define ECO_RENDER  //ECO_RENDER adds a small delay between rendering, reduce FPS in main menu or in videos
+	#define ECO_RENDER			//ECO_RENDER adds a small delay between rendering, reduce FPS in main menu or in videos
 //Renderer
 
 // VISUAL:
-	#define TREE_WIND_EFFECT								// configurable tree sway, can be used to have trees sway more during storms or lightly on clear days.
+	#define TREE_WIND_EFFECT	// configurable tree sway, can be used to have trees sway more during storms or lightly on clear days.
 //-VISUAL
+
+// Scripts
+#define SCRIPT_VARS_STORAGE		// включает хранилище скриптовых переменных в сейвы
--- SourcesAXR/xrGame/alife_storage_manager.cpp
+++ SourcesAXR/xrGame/alife_storage_manager.cpp
@@ -22,6 +22,7 @@
 #include "string_table.h"
 #include "../xrEngine/igame_persistent.h"
 #include "autosave_manager.h"
+#include "script_vars_storage.h"
 
 XRCORE_API string_path g_bug_report_file;
 
@@ -65,6 +66,10 @@ void CALifeStorageManager::save	(LPCSTR save_name_no_check, bool update_name)
 		spawns().save			(stream);
 		objects().save			(stream);
 		registry().save			(stream);
+
+#ifdef  SCRIPT_VARS_STORAGE
+		g_ScriptVars.save		(stream);
+#endif
 
 		source_count			= stream.tell();
 		void					*source_data = stream.pointer();
@@ -115,6 +120,11 @@ void CALifeStorageManager::load	(void *buffer, const u32 &buffer_size, LPCSTR fi
 	}
 
 	registry().load				(source);
+
+#ifdef  SCRIPT_VARS_STORAGE
+	if (!source.eof())
+		g_ScriptVars.load		(source);
+#endif
 
 	can_register_objects		(true);
 
