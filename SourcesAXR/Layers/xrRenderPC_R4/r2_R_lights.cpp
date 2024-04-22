2730c5dcd3a9720c26984d6e3dd88ef6c1fe1544
Dance Maniac <gazelev6@yandex.ru>
2024-04-22 17:40:54 +0300

    Использование std::move в r2_R_lights.cpp (@OpenXRay)

--- SourcesAXR/Layers/xrRenderPC_R4/r2_R_lights.cpp
+++ SourcesAXR/Layers/xrRenderPC_R4/r2_R_lights.cpp
@@ -79,7 +79,7 @@ void	CRender::render_lights	(light_Package& LP)
 			for	(u32 test=0; test<source.size(); test++)
 			{
 				light*	L	= source[test];
-				SMAP_Rect	R;
+				SMAP_Rect	R{};
 				if		(LP_smap_pool.push(R,L->X.S.size))	{
 					// OK
 					L->X.S.posX			= R.min.x;
@@ -87,14 +87,14 @@ void	CRender::render_lights	(light_Package& LP)
 					L->vis.smap_ID		= smap_ID;
 					refactored.push_back(L);
 					source.erase		(source.begin()+test);
-					test				--;
+					--test;
 				}
 			}
 		}
 
 		// save (lights are popped from back)
 		std::reverse	(refactored.begin(),refactored.end());
-		LP.v_shadowed	= refactored;
+		LP.v_shadowed	= std::move(refactored);
 	}
 
    PIX_EVENT(SHADOWED_LIGHTS);
