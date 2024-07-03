5e6d318ed832e7c857e639ee13df8fa6e8dca33d
Hrusteckiy <kostia4554t@yandex.ru>
2024-07-03 09:26:23 +0300

    ТЧ: зануление позиции окна стат еды

--- SourcesAXR/xrGameSoC/ui/UIBoosterInfo.cpp
+++ SourcesAXR/xrGameSoC/ui/UIBoosterInfo.cpp
@@ -256,7 +256,7 @@ void CUIBoosterInfo::SetInfo(CInventoryItem& pInvItem)
 
 	float val = 0.0f, max_val = 1.0f, max_value = 0.0f;
 	Fvector2 pos;
-	float h = GetWndPos().y + GetWndSize().y;
+	float h = 0.0f;
 
 	for (u32 i = 0; i < eBoostExplImmunity; ++i)
 	{
