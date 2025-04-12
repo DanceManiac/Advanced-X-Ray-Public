57154478c83ddcee56ee918f8415d496b3817e6e
Dance Maniac <gazelev6@yandex.ru>
2025-04-12 16:31:12 +0300

    Возвращён и улучшен шейдерный снегопад в зимнем режиме

--- Game/ClearSky/gamedata/shaders/r3/effects_snow.s
+++ Game/ClearSky/gamedata/shaders/r3/effects_snow.s
@@ -1,11 +1,14 @@
-function normal		(shader, t_base, t_second, t_detail)
-	shader	: begin	("stub_default","stub_default")
+function normal(shader, t_base, t_second, t_detail)
+	shader	: begin	("effects_snow","effects_snow")
 			: zb	(true,false)
 			: blend	(true,blend.srcalpha,blend.invsrcalpha)
---	TODO: DX10: implement aref for this shader
 			: aref 		(true,0)
 
-	shader 	: dx10texture	("s_base", t_base)
---	TODO: DX10: Clamp
-	shader 	: dx10sampler	("smp_base") --	Clamp
+	shader : dx10texture("s_base", "fx\\fx_snow")
+	shader : dx10texture("s_position", "$user$position")
+	shader : dx10texture("s_accumulator", "$user$accum")
+	shader : dx10texture("s_blur_4", "$user$blur_4")
+
+	shader 	: dx10sampler	("smp_rtlinear")
+	shader 	: dx10sampler	("smp_nofilter")
 end
--- SourcesAXR/Layers/xrRender/dxRainRender.cpp
+++ SourcesAXR/Layers/xrRender/dxRainRender.cpp
@@ -67,26 +67,40 @@ void dxRainRender::Render(CEffect_Rain &owner)
 {
 	float	factor				= g_pGamePersistent->Environment().CurrentEnv->rain_density;
 
-	if (factor<EPS_L)			return;
+	if (factor<EPS_L)
+		return;
 
-	float _drop_len = owner.drop_length;
-	float _drop_width = owner.drop_width;
-	float _drop_speed = 1.0f;
-	ref_shader& _splash_SH = DM_Drop->shader;
-	static shared_str s_shader_setup = "ssfx_rain_setup";
+	float	particles_multiplier	= bWinterMode ? 2.0f : 1.0f;
+	
+	float	_drop_len			= owner.drop_length;
+	float	_drop_width			= owner.drop_width;
+	float	_drop_speed			= 1.0f;
+	ref_shader&	_splash_SH		= DM_Drop->shader;
+	static shared_str s_shader_setup	= "ssfx_rain_setup";
+
+	float	wind_direction		= 0.0f;
+	float	wind_power			= 0.0f;
+	if (g_pGamePersistent->Environment().CurrentEnv->wind_velocity)
+	{
+		wind_direction	= g_pGamePersistent->Environment().CurrentEnv->wind_direction;
+		wind_power		= g_pGamePersistent->Environment().CurrentEnv->wind_velocity;
+	}
+
+	Fvector	wind_vector;
+	wind_vector.set(_sin(wind_direction), 0.0f, _cos(wind_direction));
 
 	// SSS Rain shader is available
 #if defined(USE_DX11)
-	if (ps_r4_shaders_flags.test(R4FLAG_SSS_ADDON) && RImplementation.o.ssfx_rain)
+	if (ps_r4_shaders_flags.test(R4FLAG_SSS_ADDON) && RImplementation.o.ssfx_rain && !bWinterMode)
 	{
-		_drop_len = ps_ssfx_rain_1.x;
-		_drop_width = ps_ssfx_rain_1.y;
-		_drop_speed = ps_ssfx_rain_1.z;
-		_splash_SH = SH_Splash;
+		_drop_len	= ps_ssfx_rain_1.x;
+		_drop_width	= ps_ssfx_rain_1.y;
+		_drop_speed	= ps_ssfx_rain_1.z;
+		_splash_SH	= SH_Splash;
 	}
 #endif
 
-	u32 desired_items = iFloor(0.01f * (1.f + factor * 99.0f) * m_imax_desired_items);
+	u32	desired_items	= iFloor(particles_multiplier * 0.01f * (1.f + factor * 99.0f) * m_imax_desired_items);
 
 	// Get to the desired items
 	if (current_items < desired_items)
@@ -101,35 +115,35 @@ void dxRainRender::Render(CEffect_Rain &owner)
 	float	b_radius_wrap_sqr	= _sqr((m_fsource_radius * 1.5f));
 	if (owner.items.size() < current_items)
 	{
-		// owner.items.reserve		(desired_items);
 		while (owner.items.size() < current_items)
 		{
-			CEffect_Rain::Item				one;
-			owner.Born					(one, m_fsource_radius, _drop_speed);
-			owner.items.push_back		(one);
+			CEffect_Rain::Item	one;
+			owner.Born(one, m_fsource_radius, _drop_speed);
+			owner.items.push_back(one);
 		}
 	}
 
 	// build source plane
-	Fplane src_plane;
-	Fvector norm	={0.f,-1.f,0.f};
-	Fvector upper; 	upper.set(Device.vCameraPosition.x,Device.vCameraPosition.y+m_fsource_offset,Device.vCameraPosition.z);
+	Fplane	src_plane;
+	Fvector	norm	= {0.f,-1.f,0.f};
+	Fvector	upper;
+	upper.set(Device.vCameraPosition.x,Device.vCameraPosition.y+m_fsource_offset,Device.vCameraPosition.z);
 	src_plane.build(upper,norm);
 
 	// perform update
 	u32			vOffset;
-	FVF::LIT	*verts		= (FVF::LIT	*) RCache.Vertex.Lock(desired_items*4,hGeom_Rain->vb_stride,vOffset);
+	FVF::LIT	*verts		= (FVF::LIT*)RCache.Vertex.Lock(desired_items*4,hGeom_Rain->vb_stride,vOffset);
 	FVF::LIT	*start		= verts;
 	const Fvector&	vEye	= Device.vCameraPosition;
+	
 	for (u32 I = 0; I < current_items; I++)
 	{
 		// physics and time control
-		CEffect_Rain::Item&	one		=	owner.items.at(I);
+		CEffect_Rain::Item&	one	= owner.items.at(I);
 
 		if (one.dwTime_Hit < Device.dwTimeGlobal)
 		{
 			owner.Hit(one.Phit);
-
 			if (current_items > desired_items)
 				current_items--; // Hit something
 		}
@@ -137,53 +151,62 @@ void dxRainRender::Render(CEffect_Rain &owner)
 		if (one.dwTime_Life < Device.dwTimeGlobal)
 		{
 			owner.Born(one, m_fsource_radius, _drop_speed);
-
 			if (current_items > desired_items)
-				current_items--; // Out of life ( invalidated, never hit something, etc. )
+				current_items--; // Out of life
 		}
 
-		// последн€€ дельта ??
-		//.		float xdt		= float(one.dwTime_Hit-Device.dwTimeGlobal)/1000.f;
-		//.		float dt		= Device.fTimeDelta;//xdt<Device.fTimeDelta?xdt:Device.fTimeDelta;
-		float dt		= Device.fTimeDelta;
-		one.P.mad		(one.D,one.fSpeed*dt);
+		float	dt	= Device.fTimeDelta;
+
+		if (bWinterMode)
+		{
+			Fvector	wind_effect;
+			wind_effect.mul(wind_vector, wind_power * 0.5f * dt);
+
+			Fvector	movement;
+			movement.set(one.D.x + wind_effect.x, one.D.y, one.D.z + wind_effect.z);
+			one.P.mad(movement, one.fSpeed * (0.75 + (wind_power * 0.2f)) * dt);
+		}
+		else
+			one.P.mad(one.D, one.fSpeed * dt);
 
 		Device.Statistic->TEST1.Begin();
-		Fvector	wdir;	wdir.set(one.P.x-vEye.x,0,one.P.z-vEye.z);
+		Fvector	wdir;
+		wdir.set(one.P.x-vEye.x,0,one.P.z-vEye.z);
 		float	wlen	= wdir.square_magnitude();
-		if (wlen>b_radius_wrap_sqr)	{
-			wlen		= _sqrt(wlen);
-			//.			Device.Statistic->TEST3.Begin();
-			if ((one.P.y-vEye.y)<m_fsink_offset){
-				// need born
+		if (wlen>b_radius_wrap_sqr)
+		{
+			wlen	= _sqrt(wlen);
+			if ((one.P.y-vEye.y)<m_fsink_offset)
+			{
 				one.invalidate();
-			}else{
-				Fvector		inv_dir, src_p;
+			}
+			else
+			{
+				Fvector	inv_dir, src_p;
 				inv_dir.invert(one.D);
-				wdir.div	(wlen);
-				one.P.mad	(one.P, wdir, -(wlen+m_fsource_radius));
-				if (src_plane.intersectRayPoint(one.P,inv_dir,src_p)){
-					float dist_sqr	= one.P.distance_to_sqr(src_p);
-					float height	= m_fmax_distance;
-					if (owner.RayPick(src_p,one.D,height,collide::rqtBoth)){	
-						if (_sqr(height)<=dist_sqr){ 
-							one.invalidate	();								// need born
-							//							Log("1");
-						}else{	
-							owner.RenewItem	(one,height-_sqrt(dist_sqr),TRUE);		// fly to point
-							//							Log("2",height-dist);
-						}
-					}else{
-						owner.RenewItem		(one,m_fmax_distance-_sqrt(dist_sqr),FALSE);		// fly ...
-						//						Log("3",1.5f*b_height-dist);
+				wdir.div(wlen);
+				one.P.mad(one.P, wdir, -(wlen+m_fsource_radius));
+				if (src_plane.intersectRayPoint(one.P,inv_dir,src_p))
+				{
+					float	dist_sqr	= one.P.distance_to_sqr(src_p);
+					float	height		= m_fmax_distance;
+					if (owner.RayPick(src_p,one.D,height,collide::rqtBoth))
+					{	
+						if (_sqr(height)<=dist_sqr)
+							one.invalidate();
+						else
+							owner.RenewItem(one,height-_sqrt(dist_sqr),TRUE);
 					}
-				}else{
-					// need born
+					else
+					{
+						owner.RenewItem(one,m_fmax_distance-_sqrt(dist_sqr),FALSE);
+					}
+				}
+				else
+				{
 					one.invalidate();
-					//					Log("4");
 				}
 			}
-			//.			Device.Statistic->TEST3.End();
 		}
 		Device.Statistic->TEST1.End();
 
@@ -199,21 +222,18 @@ void dxRainRender::Render(CEffect_Rain &owner)
 				pos_trail.mad(pos_head, one.D, -owner.drop_length * factor_visual);
 		}
 		else
-		{
-			if (ps_r4_shaders_flags.test(R4FLAG_SSS_ADDON))
-				pos_trail.mad(pos_head, one.D, -_drop_len * 5.5f);
-			else
-				pos_trail.mad(pos_head, one.D, -owner.drop_length * 5.5f);
-		}
+			pos_trail.mad(pos_head, one.D, -owner.drop_length * 5.5f);
 
 		// Culling
-		Fvector sC,lineD;	float sR; 
-		sC.sub			(pos_head,pos_trail);
-		lineD.normalize	(sC);
-		sC.mul			(.5f);
-		sR				= sC.magnitude();
-		sC.add			(pos_trail);
-		if (!::Render->ViewBase.testSphere_dirty(sC,sR))	continue;
+		Fvector	sC, lineD;
+		float	sR;
+		sC.sub(pos_head,pos_trail);
+		lineD.normalize(sC);
+		sC.mul(.5f);
+		sR	= sC.magnitude();
+		sC.add(pos_trail);
+		if (!::Render->ViewBase.testSphere_dirty(sC,sR))
+			continue;
 
 		constexpr Fvector2 UV[2][4]={
 			{{0,1},{0,0},{1,1},{1,0}},
@@ -221,111 +241,109 @@ void dxRainRender::Render(CEffect_Rain &owner)
 		};
 
 		// Everything OK - build vertices
-		Fvector	P,lineTop,camDir;
-		camDir.sub			(sC,vEye);
-		camDir.normalize	();
+		Fvector	P, lineTop, camDir;
+		camDir.sub(sC,vEye);
+		camDir.normalize();
 		lineTop.crossproduct(camDir,lineD);
-		float w = ps_r4_shaders_flags.test(R4FLAG_SSS_ADDON) ? _drop_width : owner.drop_width;
-		u32 s	= one.uv_set;
+		float	w	= ps_r4_shaders_flags.test(R4FLAG_SSS_ADDON) && !bWinterMode ? _drop_width : owner.drop_width;
+		u32		s	= one.uv_set;
 		P.mad(pos_trail,lineTop,-w);	verts->set(P,u_rain_color,UV[s][0].x,UV[s][0].y);	verts++;
 		P.mad(pos_trail,lineTop,w);		verts->set(P,u_rain_color,UV[s][1].x,UV[s][1].y);	verts++;
 		P.mad(pos_head, lineTop,-w);	verts->set(P,u_rain_color,UV[s][2].x,UV[s][2].y);	verts++;
 		P.mad(pos_head, lineTop,w);		verts->set(P,u_rain_color,UV[s][3].x,UV[s][3].y);	verts++;
 	}
-	u32 vCount					= (u32)(verts-start);
-	RCache.Vertex.Unlock		(vCount,hGeom_Rain->vb_stride);
+	
+	u32	vCount	= (u32)(verts-start);
+	RCache.Vertex.Unlock(vCount,hGeom_Rain->vb_stride);
 
 	// Render if needed
-	if (vCount)	{
-		//HW.pDevice->SetRenderState	(D3DRS_CULLMODE,D3DCULL_NONE);
+	if (vCount)
+	{
 		RCache.set_CullMode(CULL_NONE);
-		RCache.set_xform_world		(Fidentity);
-		RCache.set_Shader			(SH_Rain);
-		RCache.set_Geometry			(hGeom_Rain);
-		RCache.Render				(D3DPT_TRIANGLELIST,vOffset,0,vCount,0,vCount/2);
-		//HW.pDevice->SetRenderState	(D3DRS_CULLMODE,D3DCULL_CCW);
+		RCache.set_xform_world(Fidentity);
+		RCache.set_Shader(SH_Rain);
+		RCache.set_Geometry(hGeom_Rain);
+		RCache.Render(D3DPT_TRIANGLELIST,vOffset,0,vCount,0,vCount/2);
 		RCache.set_CullMode(CULL_CCW);
-		RCache.set_c(s_shader_setup, ps_ssfx_rain_2); // Alpha, Brigthness, Refraction, Reflection
+		RCache.set_c(s_shader_setup, ps_ssfx_rain_2);
 	}
 
 	// Particles
-	CEffect_Rain::Particle*	P		= owner.particle_active;
-	if (0==P)			return;
+	CEffect_Rain::Particle*	P	= owner.particle_active;
+	if (0==P)	return;
 
 	{
-		float	dt				= Device.fTimeDelta;
-		_IndexStream& _IS		= RCache.Index;
+		float	dt		= Device.fTimeDelta;
+		_IndexStream& _IS	= RCache.Index;
 
-		if (ps_r4_shaders_flags.test(R4FLAG_SSS_ADDON))
+		if (ps_r4_shaders_flags.test(R4FLAG_SSS_ADDON) && !bWinterMode)
 		{
 			RCache.set_Shader(_splash_SH);
-			RCache.set_c(s_shader_setup, ps_ssfx_rain_3); // Alpha, Refraction
+			RCache.set_c(s_shader_setup, ps_ssfx_rain_3);
 		}
 		else
 		{
 			RCache.set_Shader(DM_Drop->shader);
 		}
 
-		Fmatrix					mXform,mScale;
-		u32						pcount  = 0;
-		u32						v_offset,i_offset;
-		u32						vCount_Lock		= m_iparticles_cache*DM_Drop->number_vertices;
-		u32						iCount_Lock		= m_iparticles_cache*DM_Drop->number_indices;
-		IRender_DetailModel::fvfVertexOut* v_ptr= (IRender_DetailModel::fvfVertexOut*) RCache.Vertex.Lock	(vCount_Lock, hGeom_Drops->vb_stride, v_offset);
-		u16*					i_ptr			= _IS.Lock													(iCount_Lock, i_offset);
-		while (P)	{
-			CEffect_Rain::Particle*	next	=	P->next;
+		Fmatrix	mXform, mScale;
+		u32		pcount	= 0;
+		u32		v_offset, i_offset;
+		u32		vCount_Lock	= m_iparticles_cache*DM_Drop->number_vertices;
+		u32		iCount_Lock	= m_iparticles_cache*DM_Drop->number_indices;
+		IRender_DetailModel::fvfVertexOut* v_ptr= (IRender_DetailModel::fvfVertexOut*)RCache.Vertex.Lock(vCount_Lock, hGeom_Drops->vb_stride, v_offset);
+		u16*	i_ptr	= _IS.Lock(iCount_Lock, i_offset);
+		
+		while (P)
+		{
+			CEffect_Rain::Particle*	next	= P->next;
 
-			// Update
-			// P can be zero sometimes and it crashes
-			P->time				-=	dt;
-			if (P->time<0)	{
-				owner.p_free			(P);
-				P				=	next;
+			P->time	-= dt;
+			if (P->time<0)
+			{
+				owner.p_free(P);
+				P	= next;
 				continue;
 			}
 
-			// Render
 			if (::Render->ViewBase.testSphere_dirty(P->bounds.P, P->bounds.R))
 			{
-				// Build matrix
-				float scale			=	P->time / m_fparticles_time;
-				mScale.scale		(scale,scale,scale);
-				mXform.mul_43		(P->mXForm,mScale);
+				float	scale	= P->time/m_fparticles_time;
+				mScale.scale(scale,scale,scale);
+				mXform.mul_43(P->mXForm,mScale);
 
-				// XForm verts
-				DM_Drop->transfer	(mXform,v_ptr,u_rain_color,i_ptr,pcount*DM_Drop->number_vertices);
-				v_ptr			+=	DM_Drop->number_vertices;
-				i_ptr			+=	DM_Drop->number_indices;
-				pcount			++;
+				DM_Drop->transfer(mXform,v_ptr,u_rain_color,i_ptr,pcount*DM_Drop->number_vertices);
+				v_ptr	+= DM_Drop->number_vertices;
+				i_ptr	+= DM_Drop->number_indices;
+				pcount++;
 
-				if (pcount >= m_iparticles_cache) {
-					// flush
-					u32	dwNumPrimitives		= iCount_Lock/3;
-					RCache.Vertex.Unlock	(vCount_Lock,hGeom_Drops->vb_stride);
-					_IS.Unlock				(iCount_Lock);
-					RCache.set_Geometry		(hGeom_Drops);
-					RCache.Render			(D3DPT_TRIANGLELIST,v_offset, 0,vCount_Lock,i_offset,dwNumPrimitives);
+				if (pcount >= m_iparticles_cache)
+				{
+					u32	dwNumPrimitives	= iCount_Lock/3;
+					RCache.Vertex.Unlock(vCount_Lock,hGeom_Drops->vb_stride);
+					_IS.Unlock(iCount_Lock);
+					RCache.set_Geometry(hGeom_Drops);
+					RCache.Render(D3DPT_TRIANGLELIST,v_offset,0,vCount_Lock,i_offset,dwNumPrimitives);
 
-					v_ptr					= (IRender_DetailModel::fvfVertexOut*)			RCache.Vertex.Lock	(vCount_Lock, hGeom_Drops->vb_stride, v_offset);
-					i_ptr					= _IS.Lock										(iCount_Lock, i_offset);
+					v_ptr	= (IRender_DetailModel::fvfVertexOut*)RCache.Vertex.Lock(vCount_Lock,hGeom_Drops->vb_stride,v_offset);
+					i_ptr	= _IS.Lock(iCount_Lock,i_offset);
 
 					pcount	= 0;
 				}
 			}
 
-			P = next;
+			P	= next;
 		}
 
-		// Flush if needed
-		vCount_Lock						= pcount*DM_Drop->number_vertices;
-		iCount_Lock						= pcount*DM_Drop->number_indices;
-		u32	dwNumPrimitives				= iCount_Lock/3;
-		RCache.Vertex.Unlock			(vCount_Lock,hGeom_Drops->vb_stride);
-		_IS.Unlock						(iCount_Lock);
-		if (pcount)	{
-			RCache.set_Geometry		(hGeom_Drops);
-			RCache.Render			(D3DPT_TRIANGLELIST,v_offset,0,vCount_Lock,i_offset,dwNumPrimitives);
+		vCount_Lock		= pcount*DM_Drop->number_vertices;
+		iCount_Lock		= pcount*DM_Drop->number_indices;
+		u32	dwNumPrimitives	= iCount_Lock/3;
+		RCache.Vertex.Unlock(vCount_Lock,hGeom_Drops->vb_stride);
+		_IS.Unlock(iCount_Lock);
+		if (pcount)
+		{
+			RCache.set_Geometry(hGeom_Drops);
+			RCache.Render(D3DPT_TRIANGLELIST,v_offset,0,vCount_Lock,i_offset,dwNumPrimitives);
 		}
 	}
 }
