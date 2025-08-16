8768135fb1b5c2e4039835b0fd175e827ef8e7e3
Ceasar <50290024+CeasarLFO@users.noreply.github.com>
2025-08-16 06:40:50 +0200

    UI: Weapon params

--- SourcesAXR/xrGameCS/ui/UIWpnParams.cpp
+++ SourcesAXR/xrGameCS/ui/UIWpnParams.cpp
@@ -1,4 +1,4 @@
-#include "pch_script.h"
+﻿#include "pch_script.h"
 #include "UIWpnParams.h"
 #include "UIXmlInit.h"
 #include "../level.h"
@@ -6,6 +6,7 @@
 #include "../ai_space.h"
 #include "../../XrServerEntitiesCS/script_engine.h"
 #include "inventory_item_object.h"
+#include "UIInventoryUtilities.h"
 #include "Weapon.h"
 #include "WeaponBinoculars.h"
 #include "WeaponKnife.h"
@@ -19,6 +20,9 @@ struct SLuaWpnParams
 	luabind::functor<float>		m_functorDamage;
 	luabind::functor<float>		m_functorDamageMP;
 	luabind::functor<float>		m_functorHandling;
+	luabind::functor<float>		m_functorDamageSil;
+	luabind::functor<float>		m_functorRange;
+	luabind::functor<float>		m_functorImp;
 	SLuaWpnParams();
 	~SLuaWpnParams();
 };
@@ -26,11 +30,14 @@ struct SLuaWpnParams
 SLuaWpnParams::SLuaWpnParams()
 {
 	bool	functor_exists;
-	functor_exists	= ai().script_engine().functor("ui_wpn_params.GetRPM" ,		m_functorRPM);		VERIFY(functor_exists);
-	functor_exists	= ai().script_engine().functor("ui_wpn_params.GetDamage" ,	m_functorDamage);	VERIFY(functor_exists);
-	functor_exists	= ai().script_engine().functor("ui_wpn_params.GetDamageMP" ,m_functorDamageMP);	VERIFY(functor_exists);
-	functor_exists	= ai().script_engine().functor("ui_wpn_params.GetHandling" ,m_functorHandling);	VERIFY(functor_exists);
-	functor_exists	= ai().script_engine().functor("ui_wpn_params.GetAccuracy" ,m_functorAccuracy);	VERIFY(functor_exists);
+	functor_exists = ai().script_engine().functor("ui_wpn_params.GetRPM", m_functorRPM);			VERIFY(functor_exists);
+	functor_exists = ai().script_engine().functor("ui_wpn_params.GetDamage", m_functorDamage);		VERIFY(functor_exists);
+	functor_exists = ai().script_engine().functor("ui_wpn_params.GetDamageMP", m_functorDamageMP);		VERIFY(functor_exists);
+	functor_exists = ai().script_engine().functor("ui_wpn_params.GetHandling", m_functorHandling);		VERIFY(functor_exists);
+	functor_exists = ai().script_engine().functor("ui_wpn_params.GetAccuracy", m_functorAccuracy);		VERIFY(functor_exists);
+	functor_exists = ai().script_engine().functor("ui_wpn_params.GetDamagesil", m_functorDamageSil);	VERIFY(functor_exists);
+	functor_exists = ai().script_engine().functor("ui_wpn_params.GetRange", m_functorRange);		VERIFY(functor_exists);
+	functor_exists = ai().script_engine().functor("ui_wpn_params.GetImp", m_functorImp);			VERIFY(functor_exists);
 }
 
 SLuaWpnParams::~SLuaWpnParams()
@@ -53,11 +60,26 @@ CUIWpnParams::CUIWpnParams()
 	AttachChild(&m_textDamage);
 	AttachChild(&m_textHandling);
 	AttachChild(&m_textRPM);
+	AttachChild(&m_textDamageSil);
+	AttachChild(&m_textRange);
+	AttachChild(&m_textImp);
 
 	AttachChild(&m_progressAccuracy);
 	AttachChild(&m_progressDamage);
 	AttachChild(&m_progressHandling);
 	AttachChild(&m_progressRPM);
+	AttachChild(&m_progressDamageSil);
+	AttachChild(&m_progressRange);
+	AttachChild(&m_progressImp);
+
+	AttachChild(&m_stAmmo);
+	AttachChild(&m_textAmmoCount);
+	AttachChild(&m_textAmmoCount2);
+	AttachChild(&m_textAmmoTypes);
+	AttachChild(&m_textAmmoUsedType);
+	AttachChild(&m_stAmmoType1);
+	AttachChild(&m_stAmmoType2);
+	//	AttachChild(&m_stAmmoType3);
 }
 
 CUIWpnParams::~CUIWpnParams()
@@ -73,14 +95,33 @@ void CUIWpnParams::InitFromXml(CUIXml& xml_doc)
 	CUIXmlInit::InitStatic			(xml_doc, "wpn_params:cap_damage",			0, &m_textDamage);
 	CUIXmlInit::InitStatic			(xml_doc, "wpn_params:cap_handling",		0, &m_textHandling);
 	CUIXmlInit::InitStatic			(xml_doc, "wpn_params:cap_rpm",				0, &m_textRPM);
+	CUIXmlInit::InitStatic			(xml_doc, "wpn_params:cap_damagesil",		0, &m_textDamageSil);
+	CUIXmlInit::InitStatic			(xml_doc, "wpn_params:cap_Imp",				0, &m_textRange);
+	CUIXmlInit::InitStatic			(xml_doc, "wpn_params:cap_Range",			0, &m_textImp);
 
-	m_progressAccuracy.InitFromXml	( xml_doc, "wpn_params:progress_accuracy" );
-	m_progressDamage.InitFromXml	( xml_doc, "wpn_params:progress_damage" );
-	m_progressHandling.InitFromXml	( xml_doc, "wpn_params:progress_handling" );
-	m_progressRPM.InitFromXml		( xml_doc, "wpn_params:progress_rpm" );
+	m_progressAccuracy.InitFromXml	(xml_doc, "wpn_params:progress_accuracy" );
+	m_progressDamage.InitFromXml	(xml_doc, "wpn_params:progress_damage" );
+	m_progressHandling.InitFromXml	(xml_doc, "wpn_params:progress_handling" );
+	m_progressRPM.InitFromXml		(xml_doc, "wpn_params:progress_rpm" );
+	m_progressDamageSil.InitFromXml	(xml_doc, "wpn_params:progress_damagesil");
+	m_progressRange.InitFromXml		(xml_doc, "wpn_params:progress_Range");
+	m_progressImp.InitFromXml		(xml_doc, "wpn_params:progress_Imp");
+
+
+	if (IsGameTypeSingle())
+	{
+		CUIXmlInit::InitStatic(xml_doc, "wpn_params:static_ammo",			0, &m_stAmmo);
+		CUIXmlInit::InitStatic(xml_doc, "wpn_params:cap_ammo_count",		0, &m_textAmmoCount);
+		CUIXmlInit::InitStatic(xml_doc, "wpn_params:cap_ammo_count2",		0, &m_textAmmoCount2);
+		CUIXmlInit::InitStatic(xml_doc, "wpn_params:cap_ammo_types",		0, &m_textAmmoTypes);
+		CUIXmlInit::InitStatic(xml_doc, "wpn_params:cap_ammo_used_type",	0, &m_textAmmoUsedType);
+		CUIXmlInit::InitStatic(xml_doc, "wpn_params:static_ammo_type1",		0, &m_stAmmoType1);
+		CUIXmlInit::InitStatic(xml_doc, "wpn_params:static_ammo_type2",		0, &m_stAmmoType2);
+	//	CUIXmlInit::InitStatic(xml_doc, "wpn_params:static_ammo_type3",		0, &m_stAmmoType3);
+	}
 }
 
-void CUIWpnParams::SetInfo( CInventoryItem const* slot_wpn, CInventoryItem const& cur_wpn )
+void CUIWpnParams::SetInfo(CInventoryItem* slot_wpn, CInventoryItem& cur_wpn)
 {
 	if ( !g_lua_wpn_params )
 	{
@@ -92,17 +133,23 @@ void CUIWpnParams::SetInfo( CInventoryItem const* slot_wpn, CInventoryItem const
 	str_upgrades[0] = 0;
 	cur_wpn.get_upgrades_str( str_upgrades );
 
-	float cur_rpm    = g_lua_wpn_params->m_functorRPM( cur_section, str_upgrades );
-	float cur_accur  = g_lua_wpn_params->m_functorAccuracy( cur_section, str_upgrades );
-	float cur_hand   = g_lua_wpn_params->m_functorHandling( cur_section, str_upgrades );
-	float cur_damage = ( GameID() == eGameIDSingle ) ?
+	float cur_rpm		= g_lua_wpn_params->m_functorRPM( cur_section, str_upgrades );
+	float cur_accur		= g_lua_wpn_params->m_functorAccuracy( cur_section, str_upgrades );
+	float cur_hand		= g_lua_wpn_params->m_functorHandling( cur_section, str_upgrades );
+	float cur_Range		= g_lua_wpn_params->m_functorRange(cur_section, str_upgrades);
+	float cur_Imp		= g_lua_wpn_params->m_functorImp(cur_section, str_upgrades);
+	float cur_damagesil = g_lua_wpn_params->m_functorDamageSil(cur_section, str_upgrades);
+	float cur_damage	= ( GameID() == eGameIDSingle ) ?
 		g_lua_wpn_params->m_functorDamage( cur_section, str_upgrades )
 		: g_lua_wpn_params->m_functorDamageMP( cur_section, str_upgrades );
 
-	float slot_rpm    = cur_rpm;
-	float slot_accur  = cur_accur;
-	float slot_hand   = cur_hand;
-	float slot_damage = cur_damage;
+	float slot_rpm			= cur_rpm;
+	float slot_accur		= cur_accur;
+	float slot_hand			= cur_hand;
+	float slot_damage		= cur_damage;
+	float slot_damagesil	= cur_damagesil;
+	float slot_range		= cur_Range;
+	float slot_imp			= cur_Imp;
 
 	if ( slot_wpn && (slot_wpn != &cur_wpn) )
 	{
@@ -110,18 +157,24 @@ void CUIWpnParams::SetInfo( CInventoryItem const* slot_wpn, CInventoryItem const
 		str_upgrades[0] = 0;
 		slot_wpn->get_upgrades_str( str_upgrades );
 
-		slot_rpm    = g_lua_wpn_params->m_functorRPM( slot_section, str_upgrades );
-		slot_accur  = g_lua_wpn_params->m_functorAccuracy( slot_section, str_upgrades );
-		slot_hand   = g_lua_wpn_params->m_functorHandling( slot_section, str_upgrades );
-		slot_damage = ( GameID() == eGameIDSingle ) ?
+		slot_rpm		= g_lua_wpn_params->m_functorRPM( slot_section, str_upgrades );
+		slot_accur		= g_lua_wpn_params->m_functorAccuracy( slot_section, str_upgrades );
+		slot_hand		= g_lua_wpn_params->m_functorHandling( slot_section, str_upgrades );
+		slot_range		= g_lua_wpn_params->m_functorRange(slot_section, str_upgrades);
+		slot_imp		= g_lua_wpn_params->m_functorImp(slot_section, str_upgrades);
+		slot_damagesil	= g_lua_wpn_params->m_functorDamageSil(slot_section, str_upgrades);
+		slot_damage		= ( GameID() == eGameIDSingle ) ?
 			g_lua_wpn_params->m_functorDamage( slot_section, str_upgrades )
 			: g_lua_wpn_params->m_functorDamageMP( slot_section, str_upgrades );
 	}
 	
-	m_progressAccuracy.SetTwoPos( cur_accur,  slot_accur );
-	m_progressDamage.SetTwoPos(   cur_damage, slot_damage );
-	m_progressHandling.SetTwoPos( cur_hand,   slot_hand );
-	m_progressRPM.SetTwoPos(      cur_rpm,    slot_rpm );
+	m_progressAccuracy.SetTwoPos	(cur_accur,  slot_accur );
+	m_progressDamage.SetTwoPos		(cur_damage, slot_damage );
+	m_progressHandling.SetTwoPos	(cur_hand,   slot_hand );
+	m_progressRPM.SetTwoPos			(cur_rpm,    slot_rpm );
+	m_progressDamageSil.SetTwoPos	(cur_damagesil, slot_damagesil);
+	m_progressRange.SetTwoPos		(cur_Range, slot_range);
+	m_progressImp.SetTwoPos			(cur_Imp, slot_imp);
 
 	const bool showAmmo = READ_IF_EXISTS(pSettings, r_bool, cur_section, "show_ammo", true);
 	
@@ -129,6 +182,111 @@ void CUIWpnParams::SetInfo( CInventoryItem const* slot_wpn, CInventoryItem const
 	m_progressAccuracy.Show(showAmmo);
 	m_textAccuracy.Show(showAmmo);
 	m_textRPM.Show(showAmmo);
+
+	if (IsGameTypeSingle())
+	{
+		// Lex Addon (correct by Suhar_) 7.08.2018		(begin)
+		// Инициализируем переменную используемых оружием патронов
+		xr_vector<shared_str> ammo_types;
+
+		CWeapon* weapon = cur_wpn.cast_weapon();
+
+		if (!weapon)
+			return;
+
+		m_stAmmo.Show(showAmmo);
+		m_textAmmoCount.Show(showAmmo);
+		m_textAmmoCount2.Show(showAmmo);
+		m_textAmmoTypes.Show(showAmmo);
+		m_textAmmoUsedType.Show(showAmmo);
+		m_stAmmoType1.Show(showAmmo);
+		m_stAmmoType2.Show(showAmmo);
+	//	m_stAmmoType3.Show(showAmmo);
+		/*
+		m_stAmmoType4.Show(showAmmo);
+		m_stAmmoType5.Show(showAmmo);
+		m_stAmmoType6.Show(showAmmo);*/
+
+		if (!showAmmo)
+			return;
+
+		int ammo_count = weapon->GetAmmoMagSize();
+		int ammo_count2 = ammo_count;
+
+		if (slot_wpn)
+		{
+			CWeapon* slot_weapon = slot_wpn->cast_weapon();
+			if (slot_weapon)
+				ammo_count2 = slot_weapon->GetAmmoMagSize();
+		}
+
+		if (ammo_count == ammo_count2)
+			m_textAmmoCount2.SetTextColor(color_rgba(170, 170, 170, 255));
+		else if (ammo_count < ammo_count2)
+			m_textAmmoCount2.SetTextColor(color_rgba(255, 0, 0, 255));
+		else
+			m_textAmmoCount2.SetTextColor(color_rgba(0, 255, 0, 255));
+
+		string128 str;
+		xr_sprintf(str, sizeof(str), "%d", ammo_count);
+		m_textAmmoCount2.SetText(str);
+
+		ammo_types = weapon->m_ammoTypes;
+		if (ammo_types.empty())
+			return;
+		// Получаем количчество видов используемых оружием патронов
+		ammo_types_size = ammo_types.size();
+
+		xr_sprintf(str, sizeof(str), "%s", pSettings->r_string(ammo_types[0].c_str(), "inv_name_short"));
+		m_textAmmoUsedType.SetTextST(str);
+
+		if (pSettings->line_exist(ammo_types[0].c_str(), "icons_texture"))
+		{
+			LPCSTR icons_texture = pSettings->r_string(ammo_types[0].c_str(), "icons_texture");
+			m_stAmmoType1.SetShader(InventoryUtilities::GetCustomIconTextureShader(icons_texture));
+		}
+		else
+		{
+			m_stAmmoType1.SetShader(InventoryUtilities::GetEquipmentIconsShader());
+		}
+
+
+
+
+		Frect				tex_rect;
+		tex_rect.x1 = float(pSettings->r_u32(ammo_types[0].c_str(), "inv_grid_x") * UI().inv_grid_kx());
+		tex_rect.y1 = float(pSettings->r_u32(ammo_types[0].c_str(), "inv_grid_y") * UI().inv_grid_kx());
+		tex_rect.x2 = float(pSettings->r_u32(ammo_types[0].c_str(), "inv_grid_width") * UI().inv_grid_kx());
+		tex_rect.y2 = float(pSettings->r_u32(ammo_types[0].c_str(), "inv_grid_height") * UI().inv_grid_kx());
+		tex_rect.rb.add(tex_rect.lt);
+		m_stAmmoType1.SetOriginalRect(tex_rect);
+		m_stAmmoType1.TextureOn();
+		m_stAmmoType1.SetStretchTexture(true);
+
+		m_stAmmoType1.SetWndSize(Fvector2().set((tex_rect.x2 - tex_rect.x1)* UI().get_current_kx()* (1 / UI().get_icons_kx()), (tex_rect.y2 - tex_rect.y1)* (1 / UI().get_icons_kx())));
+
+		m_stAmmoType2.SetShader(InventoryUtilities::GetEquipmentIconsShader());
+		if (ammo_types.size() == 1)
+		{
+			tex_rect.set(0, 0, 1, 1);
+		}
+		else
+		{
+			tex_rect.x1 = float(pSettings->r_u32(ammo_types[1].c_str(), "inv_grid_x") * UI().inv_grid_kx());
+			tex_rect.y1 = float(pSettings->r_u32(ammo_types[1].c_str(), "inv_grid_y") * UI().inv_grid_kx());
+			tex_rect.x2 = float(pSettings->r_u32(ammo_types[1].c_str(), "inv_grid_width") * UI().inv_grid_kx());
+			tex_rect.y2 = float(pSettings->r_u32(ammo_types[1].c_str(), "inv_grid_height") * UI().inv_grid_kx());
+			tex_rect.rb.add(tex_rect.lt);
+		}
+		m_stAmmoType2.SetOriginalRect(tex_rect);
+		m_stAmmoType2.TextureOn();
+		m_stAmmoType2.SetStretchTexture(true);
+
+		m_stAmmoType2.SetWndSize(Fvector2().set((tex_rect.x2 - tex_rect.x1) * UI().get_current_kx() * (1 / UI().get_icons_kx()), (tex_rect.y2 - tex_rect.y1) * (1 / UI().get_icons_kx())));
+
+	}
+
+
 }
 
 bool CUIWpnParams::Check(CInventoryItem& wpn_section)
--- SourcesAXR/xrGameCS/ui/UIWpnParams.h
+++ SourcesAXR/xrGameCS/ui/UIWpnParams.h
@@ -17,19 +17,35 @@ public:
 	virtual					~CUIWpnParams		();
 
 	void 					InitFromXml			(CUIXml& xml_doc);
-	void					SetInfo				(CInventoryItem const* slot_wpn, CInventoryItem const& cur_wpn);
+	void					SetInfo				(CInventoryItem* slot_wpn, CInventoryItem& cur_wpn);
 	bool 					Check				(CInventoryItem& wpn_section);
+	int						ammo_types_size;	//Lex Addon
 
 protected:
 	CUIDoubleProgressBar	m_progressAccuracy; // red or green
 	CUIDoubleProgressBar	m_progressHandling;
 	CUIDoubleProgressBar	m_progressDamage;
 	CUIDoubleProgressBar	m_progressRPM;
+	CUIDoubleProgressBar	m_progressRange;
+	CUIDoubleProgressBar	m_progressDamageSil;
+	CUIDoubleProgressBar	m_progressImp;
 
 	CUIStatic				m_textAccuracy;
 	CUIStatic				m_textHandling;
 	CUIStatic				m_textDamage;
 	CUIStatic				m_textRPM;
+	CUIStatic				m_textDamageSil;
+	CUIStatic				m_textRange;
+	CUIStatic				m_textImp;
+
+	CUIStatic				m_stAmmo;
+	CUIStatic				m_textAmmoTypes;
+	CUIStatic				m_textAmmoUsedType;
+	CUIStatic				m_textAmmoCount;
+	CUIStatic				m_textAmmoCount2;
+	CUIStatic				m_stAmmoType1;
+	CUIStatic				m_stAmmoType2;
+	//	CUIStatic				m_stAmmoType3;
 };
 
 // -------------------------------------------------------------------------------------------------
